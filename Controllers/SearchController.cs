4aed51de074750d0ef41439fd3ec8285db99f7b9
fgeorgantzos <fgeorgantzos@novibet.com>
2023-12-26 22:14:27 +0200

    Correcting SearchCriteria properties, search api routing url and adding user agent header to handle udemy-scraping

--- Controllers/SearchController.cs
+++ Controllers/SearchController.cs
@@ -1,4 +1,3 @@
-//using Data;
 using HtmlAgilityPack;
 using System.Linq;
 using System.Net.Http;
@@ -7,11 +6,12 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Thesis.courseWebApp.Backend.Data;
 using Thesis.courseWebApp.Backend.Models;
+using System.Net;
 
 namespace Thesis.courseWebApp.Backend.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api")]
     public class SearchController : ControllerBase
     {
         private readonly AppDbContext _dbContext;
@@ -25,23 +25,33 @@ namespace Thesis.courseWebApp.Backend.Controllers
         public async Task<IActionResult> Search([FromBody] SearchCriteria criteria)      
         {
             // existing search logic with database queries
-            var resultsFromDatabase = _dbContext.Courses
-                .Where(c => c.Level == criteria.Level && c.Subjects.Contains(criteria.Subjects) && c.Duration == criteria.Duration)
-                // Add more conditions based on your search criteria
-                .ToList();
+            //var resultsFromDatabase = _dbContext.Courses
+            //    .Where(c => c.Level == criteria.Level && criteria.Subjects.Any(s => c.Subjects.Contains(s)) && c.Duration == criteria.Duration)
+            //    .ToList();
 
 
-            // Web scraping logic to get results from e-course sites
-            //var resultsFromWeb = await GetResultsFromWeb(level, subjects, duration, onSale, rating, priceRange, certification, language, courseFormat, location);
-            var resultsFromWeb = await GetResultsFromWeb(criteria);
+            try
+            {
+                //Web scraping logic to get results from e-course sites
+                var resultsFromWeb = await GetResultsFromWeb(criteria);
 
-            // Combine results from the database and web scraping
-            var allResults = resultsFromDatabase.Concat(resultsFromWeb).ToList();
+                //// Combine results from the database and web scraping
+                //var allResults = resultsFromDatabase.Concat(resultsFromWeb).ToList();
 
-            return Ok(allResults);
+                //return Ok(allResults);
+
+                return Ok(new { Message = "Success", Results = resultsFromWeb });
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions and return an appropriate error response
+                return StatusCode(500, new { Message = "Internal Server Error", Error = ex.Message
+            });
         }
+    }
 
-        private async Task<List<Course>> GetResultsFromWeb(SearchCriteria criteria) {
+        private async Task<List<Course>> GetResultsFromWeb(SearchCriteria criteria)
+        {
             // Implement web scraping logic here
             // Example using HtmlAgilityPack and Udemy as a reference (modify as needed)
             var udemyResults = await ScrapeUdemy(criteria);
@@ -61,33 +71,44 @@ namespace Thesis.courseWebApp.Backend.Controllers
             // Send HTTP request and load HTML content
             using (var httpClient = new HttpClient())
             {
-                var htmlContent = await httpClient.GetStringAsync(udemyUrl);
+                // Add User-Agent header
+                httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36");
 
-                // Parse HTML content using HtmlAgilityPack
-                var htmlDocument = new HtmlDocument();
-                htmlDocument.LoadHtml(htmlContent);
-
-                // Extract course information from HTML nodes
-                // Modify this part based on the actual structure of Udemy's search results page
-                var courseNodes = htmlDocument.DocumentNode.SelectNodes("//div[@class='course-card--container']");
-                if (courseNodes != null)
+                try
                 {
-                    foreach (var courseNode in courseNodes)
+                    var htmlContent = await httpClient.GetStringAsync(udemyUrl);
+
+                    // Parse HTML content using HtmlAgilityPack
+                    var htmlDocument = new HtmlDocument();
+                    htmlDocument.LoadHtml(htmlContent);
+
+                    // Extract course information from HTML nodes
+                    // Modify this part based on the actual structure of Udemy's search results page
+                    var courseNodes = htmlDocument.DocumentNode.SelectNodes("//div[@class='course-card--container']");
+                    if (courseNodes != null)
                     {
-                        var courseTitle = courseNode.SelectSingleNode(".//h4")?.InnerText.Trim();
-                        var courseLink = courseNode.SelectSingleNode(".//a")?.GetAttributeValue("href", "");
-
-                        // Add more fields as needed
-
-                        var udemyCourse = new Course
+                        foreach (var courseNode in courseNodes)
                         {
-                            Title = courseTitle,
-                            Link = courseLink,
-                            // Add more fields
-                        };
+                            var courseTitle = courseNode.SelectSingleNode(".//h4")?.InnerText.Trim();
+                            var courseLink = courseNode.SelectSingleNode(".//a")?.GetAttributeValue("href", "");
 
-                        udemyResults.Add(udemyCourse);
+                            // Add more fields as needed
+
+                            var udemyCourse = new Course
+                            {
+                                Title = courseTitle,
+                                Link = courseLink,
+                                // Add more fields
+                            };
+
+                            udemyResults.Add(udemyCourse);
+                        }
                     }
+                }
+                catch (HttpRequestException ex) when (ex.StatusCode == HttpStatusCode.Forbidden)
+                {
+                    // Handle 403 Forbidden error
+                    return udemyResults; // or handle it accordingly
                 }
             }
 
--- Models/SearchCriteria.cs
+++ Models/SearchCriteria.cs
@@ -2,15 +2,15 @@ namespace Thesis.courseWebApp.Backend.Models
 {
     public class SearchCriteria
     {
-        // Properties for login data
+        // Properties for search criteria
         public string Level { get; set; }
-        public string Subjects { get; set; }
+        public string[] Subjects { get; set; }
         public string Duration { get; set; }
         public bool OnSale { get; set; }
-        public string Rating { get; set; }
-        public string PriceRange { get; set; }
+        public float Rating { get; set; }
+        public float[] PriceRange { get; set; }
         public bool Certification { get; set; }
-        public string Language { get; set; }
+        public string[] Language { get; set; }
         public string CourseFormat { get; set; }
         public string Location { get; set; }
 
