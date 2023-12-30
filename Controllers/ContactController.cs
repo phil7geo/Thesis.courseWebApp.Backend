c73da25c81e5d51fc7586925d855a67e3400532f
fgeorgantzos <fgeorgantzos@novibet.com>
2023-12-30 11:40:08 +0200

    Contact controller filling

--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -6,18 +6,53 @@ namespace Thesis.courseWebApp.Backend.Controllers
 {
 
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api")]
     public class ContactController : ControllerBase
     {
-        [HttpPost("contact/send-message")]
-        public IActionResult SendMessage([FromBody] ContactMessageModel model)
+        //private readonly ContactMessageRepository _messageRepository;
+        //private readonly EmailService _emailService;
+
+        public ContactController( )
         {
-            // contact message handling logic goes here
-            // Save the message to a database, send an email, etc.
-
-            // For simplicity, let's assume the message is sent successfully
-            return Ok(new { Message = "Message sent successfully" });
+            //_messageRepository = messageRepository;
+            //_emailService = emailService;
         }
-    }
 
+        [HttpPost("contact/send-message")]
+        public async Task<IActionResult> SendMessage([FromBody] ContactMessageModel model)
+        {
+            try
+            {
+                // Validate the model
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(new { Message = "Invalid message format" });
+                }
+
+                // Check if the email already exists in the database
+                //if (_messageRepository.EmailExists(model.Email))
+                if (model.Email == null)
+                {
+                    return BadRequest(new { Message = "Email already exists" });
+                }
+
+                // Save the message to the database
+                //var messageId = _messageRepository.SaveMessage(model);
+
+                var message = model.Message;
+
+                // Send an email (assuming you have an email service)
+                //_emailService.SendEmail(model.Email, "Subject", "Body");
+
+                // For simplicity, let's assume the message is sent successfully
+                return Ok(new { Success = true, Message = "Message sent successfully", MessageSent = message });
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions appropriately, log them, and return a meaningful error response
+                return StatusCode(500, new { Message = "Internal Server Error", Error = ex.Message });
+            }
+        }
+
+    }
 }
--- Models/ContactMessageModel.cs
+++ Models/ContactMessageModel.cs
@@ -2,10 +2,14 @@ namespace Thesis.courseWebApp.Backend.Models
 {
     public class ContactMessageModel
     {
-        // Properties for login data
+        //[Required(ErrorMessage = "FullName is required")]
+        //[FullName(ErrorMessage = "Invalid full name")]
         public string FullName { get; set; }
+
         public string Email { get; set; }
+
         public int PhoneNumber { get; set; }
-        // Add other properties as needed
+
+        public string Message { get; set; }
     }
 }
